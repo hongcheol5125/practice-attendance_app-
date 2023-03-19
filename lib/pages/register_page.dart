f4d4e1635e301dc2d9000e6243e0e0d888446bf2
hongcheol5125 <hongcheol6689@gmail.com>
2023-03-19 19:07:47 +0900

    뭐가 잘못 된거지...?

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -8,6 +10,32 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  TextEditingController _idController = TextEditingController();
+  TextEditingController _pwController = TextEditingController();
+  TextEditingController _confirmPwController = TextEditingController();
+  final String id = '';
+  final String password = '';
+
+  
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+  Future<void> addUser() async{
+      return users
+          .add({
+            'id': id,
+            'password': password, 
+          })
+          .then((value) => print("User Added"))
+          .catchError((error) => print("Failed to add user: $error"));
+    }
+
+showSnackBar(String text) {
+  SnackBar snackBar = SnackBar(
+    content: Text(text),
+  );
+ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,33 +43,34 @@ class _RegisterPageState extends State<RegisterPage> {
         backgroundColor: Colors.orange,
         title: Text('회원가입'),
       ),
-      body: GestureDetector(
-        onTap:() => FocusScope.of(context).unfocus(),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      'ID',
-                      style: TextStyle(
-                          color: Colors.orange[800],
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18),
-                    ),
-                  ],
+      body: Expanded(
+        child: GestureDetector(
+          onTap:() => FocusScope.of(context).unfocus(),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20,
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-                child: Expanded(
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'ID',
+                        style: TextStyle(
+                            color: Colors.orange[800],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0, right: 8.0),
                   child: TextField(
+                    controller: _idController,
                     decoration: InputDecoration(
                       hintText: 'ID를 입력해 주세요',
                       border: OutlineInputBorder(
@@ -50,27 +79,26 @@ class _RegisterPageState extends State<RegisterPage> {
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 15),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Password',
-                      style: TextStyle(
-                          color: Colors.orange[800],
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18),
-                    ),
-                  ],
+                SizedBox(height: 15),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Password',
+                        style: TextStyle(
+                            color: Colors.orange[800],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-                child: Expanded(
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0, right: 8.0),
                   child: TextField(
+                    controller: _pwController,
                     decoration: InputDecoration(
                       hintText: 'Password를 입력해 주세요',
                       border: OutlineInputBorder(
@@ -79,27 +107,26 @@ class _RegisterPageState extends State<RegisterPage> {
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 15),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Password 확인',
-                      style: TextStyle(
-                          color: Colors.orange[800],
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18),
-                    ),
-                  ],
+                SizedBox(height: 15),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Password 확인',
+                        style: TextStyle(
+                            color: Colors.orange[800],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-                child: Expanded(
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0, right: 8.0),
                   child: TextField(
+                    controller: _confirmPwController,
                     decoration: InputDecoration(
                       hintText: 'Password를 다시 입력해 주세요',
                       border: OutlineInputBorder(
@@ -108,17 +135,28 @@ class _RegisterPageState extends State<RegisterPage> {
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 20),
-              MaterialButton(
-                color: Colors.orange,
-                child: Text('회원가입'),
-                onPressed: () {},
-              ),
-            ],
+                SizedBox(height: 20),
+                MaterialButton(
+                  color: Colors.orange,
+                  child: Text('회원가입'),
+                  onPressed: () {
+                    if(_idController == '' || _pwController == '') {
+                    showSnackBar("빈 칸을 입력해주세요");
+                    }
+                    if(addUser == _idController) {
+                      showSnackBar('이미 존재하는 아이디입니다.');
+                    }
+                    
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
+
+
+
