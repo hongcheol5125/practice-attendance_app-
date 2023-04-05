6fa47b71e53f5303132451b473330aa17502464e
hongcheol5125 <hongcheol6689@gmail.com>
2023-04-05 13:57:02 +0900

    학생 QR페이지 꾸밈

--- lib/pages/qr_page(T).dart
+++ lib/pages/qr_page(T).dart
@@ -1,14 +1,53 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-class QrPageTeacher extends StatelessWidget {
+class QrPageTeacher extends StatefulWidget {
   const QrPageTeacher({super.key});
 
   @override
+  State<QrPageTeacher> createState() => _QrPageTeacherState();
+}
+
+class _QrPageTeacherState extends State<QrPageTeacher> {
+  String randomNumbers = '1';
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('QR Page(T)')
+      appBar: AppBar(
+        title: Text('QR 생성기'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            randomNumbers,
+            style: TextStyle(fontSize: 50, fontWeight: FontWeight.w500),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  return onRandomNumberGenerate();
+                },
+                child: Text('랜덤숫자 생성!'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
+  }
+
+  void onRandomNumberGenerate() {
+    final rand = Random();
+    final number = rand.nextInt(10000);
+
+    setState(() {
+      randomNumbers = number.toString();
+    });
   }
 }
--- lib/pages/qr_page.dart
+++ lib/pages/qr_page.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
 
-class QrPage extends StatelessWidget {
+class QrPage extends StatefulWidget {
   const QrPage({super.key});
 
   @override
+  State<QrPage> createState() => _QrPageState();
+}
+
+class _QrPageState extends State<QrPage> {
+  TextEditingController qrNumberController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('QR Page'),
-      )
-    );
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 70.0),
+          child: TextField(
+            controller: qrNumberController,
+            decoration: InputDecoration(
+              hintText: 'write down the qrnumber',
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: BorderSide(color: Colors.orange, width: 3.0),
+              )),
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: Text('출석!',
+          ),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.orange,
+          ),
+        ),
+      ],
+    ));
   }
 }
